0e1bd8b639376d5f772eba234b87e0836fb96e49
wahaj248 <wahajkhan2488@gmail.com>
2023-10-17 20:44:05 +0500

    Message

---
+++ app/page.js
@@ -0,0 +1,15 @@
+"use client"
+import Image from 'next/image'
+import styles from './page.module.css'
+import Header from './../components/header/Header'
+import Navbar from '@/components/Navbar/Navbar'
+export default function Home() {
+  return (
+  <>
+  <Header/>
+  <Navbar/>
+  </>
+  
+  
+  )
+}
